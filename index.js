81ae19ac9051a7e8de7f46826c59a9e5e2fe203c
Syed Monirul Alam <Rambling.ctg@gmail.com>
2022-05-26 02:03:53 +0600

    verify admin check api

--- index.js
+++ index.js
@@ -158,6 +158,15 @@ async function run() {
       res.send(result);
     });
 
+    // Get  Admins
+    app.get('/user/admin/:email', verifyJWT, async (req, res) => {
+      const email = req.params.email;
+      const user = await userCollection.findOne({ email: email });
+      const isAdmin = user?.role === 'admin';
+      // console.log(isAdmin);
+      res.send({ admin: isAdmin });
+    });
+
     // Purchase Post API
     app.post('/purchase', verifyJWT, async (req, res) => {
       const purchase = req.body;
