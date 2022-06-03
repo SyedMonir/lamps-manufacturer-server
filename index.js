abe26e2b89678eb7c41a1012bdc8dd9df501a044
Syed Monirul Alam <Rambling.ctg@gmail.com>
2022-06-03 10:56:49 +0600

    fixing cors

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({ origin: 'https://lamps-manufacturer.web.app/' }));
 app.use(express.json());
 
 // Verify Token
