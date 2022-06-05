0f9efd8558e887d9d7ff7ee0622253b7cfdde8ce
Syed Monirul Alam <Rambling.ctg@gmail.com>
2022-06-05 18:41:15 +0600

    fixing again

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+// app.use(cors());
+app.use(cors({ origin: 'http://lamps-manufacturer.herokuapp.com/' }));
 app.use(express.json());
 
 // Verify Token
