2c62bfcaf53ab888fe41b747ef525f98515863d0
Syed Monirul Alam <Rambling.ctg@gmail.com>
2022-06-07 23:18:44 +0600

    fixing cors issue

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 5000;
 
 // Middleware
 // app.use(cors());
-app.use(cors({ origin: 'https://lamps-manufacturer.herokuapp.com/' }));
+app.use(cors());
 app.use(express.json());
 
 // Verify Token
